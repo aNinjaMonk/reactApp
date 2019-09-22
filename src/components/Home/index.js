a66b85aac76de7064af2c63f8f2144b7197afcc7
Abhijeet <ninjamonk@protonmail.com>
2019-09-23 00:09:54 +0530

    Improved on netlify forms.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -53,7 +53,7 @@ class Index extends React.Component {
         <Destinations />
         <br /><br />
         <Activities />
-        <form name="contact" netlify>
+        <form name="contact" data-netlify="true" method="POST">
           <p>
             <label>Name: <input type="text" name="name" /></label>
           </p>
